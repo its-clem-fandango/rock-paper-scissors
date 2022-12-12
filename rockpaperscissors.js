c42df243803fe3f5336a4e6f5218054d415ae2c5
Juneau Dahl <drosshere@gmail.com>
2022-12-11 18:30:22 -0800

    Add playRound function with if...else conditional

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -14,14 +14,12 @@
 //rock-paper-scissors
     //computer randomly selects between rock-paper-scissors
 
-const randomChoice = ["rock", "paper", "scissors"];
+const randomChoice = ["rock", "paper", "scissors"]; //this is an array with strings. Arrays are used to store multiple values in a single variable. 
 
 function getComputerChoice(){
-    let choice = randomChoice[Math.floor(Math.random() * randomChoice.length)];
-    return choice;
+    let choice = randomChoice[Math.floor(Math.random() * randomChoice.length)]; //*randomchoice acts as the argument for getcompututerchoice(parameter)
+    return choice; //to store the randomly selected variable we created a local variable called choice
 }
-
-console.log(getComputerChoice());
     //once selection is made check it against the user input
     //if user selects rock then computer needs to select paper to win
     //if user selects paper then computer needs to select scissors to win
@@ -35,3 +33,16 @@ console.log(getComputerChoice());
     //declare winner or draw
 
     //repeat 5 times
+        //simplify it first by making it run once
+function playRound(playerSelection, computerSelection){
+    if (computerSelection === "paper")
+    return ("You lose! Paper beats rock.");
+else if (computerSelection === "scissors")
+    return ("You win! Rock beats scissors");
+else
+    return ("It's a draw!"); 
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
